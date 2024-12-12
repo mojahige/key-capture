0dd1489871cdede9d6e2471941785ae551e25f6d
mojahige <13248811+mojahige@users.noreply.github.com>
2024-12-12 11:53:30 +0900

    use ESLint Stylistic

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,32 @@
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import pluginVitest from '@vitest/eslint-plugin'
+import pluginVue from "eslint-plugin-vue";
+import vueTsEslintConfig from "@vue/eslint-config-typescript";
+import pluginVitest from "@vitest/eslint-plugin";
+import pluginStylistic from "@stylistic/eslint-plugin";
 
 export default [
+  pluginStylistic.configs.customize({
+    semi: true,
+    quotes: "double",
+  }),
+
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    name: "app/files-to-lint",
+    plugins: {
+      "@stylistic": pluginStylistic,
+    },
+    files: ["**/*.{js,ts,mts,tsx,vue}"],
   },
 
   {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    name: "app/files-to-ignore",
+    ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**", "src-tauri/**"],
   },
 
-  ...pluginVue.configs['flat/essential'],
+  ...pluginVue.configs["flat/essential"],
   ...vueTsEslintConfig(),
-  
+
   {
     ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
+    files: ["src/**/__tests__/*"],
   },
-]
+];
