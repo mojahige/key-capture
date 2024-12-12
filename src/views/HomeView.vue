edf48af0696c8e95a333db4bc5035955acf075a0
mojahige <13248811+mojahige@users.noreply.github.com>
2024-12-12 12:25:41 +0900

    reset src

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>This is an home page</h1>
 </template>
