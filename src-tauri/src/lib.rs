acca13b98bb23e2a1a2ba904d15266f561e28c2c
mojahige <13248811+mojahige@users.noreply.github.com>
2024-12-12 23:40:22 +0900

    add keypress_listener

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,40 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
+use device_query::{DeviceQuery, DeviceState};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+use tauri::Emitter;
+
+#[tauri::command]
+fn keypress_listener(app_handle: tauri::AppHandle) {
+    let device_state = DeviceState::new();
+    let app_handle = Arc::new(app_handle);
+
+    thread::spawn(move || {
+        let last_keys = Arc::new(Mutex::new(vec![]));
+
+        loop {
+            let current_keys = device_state.get_keys();
+            let mut last_keys_guard = last_keys.lock().unwrap();
+
+            for key in &current_keys {
+                if !last_keys_guard.contains(key) {
+                    app_handle.emit("pressed", format!("{:?}", key)).unwrap();
+                }
+            }
+            *last_keys_guard = current_keys;
+
+            thread::sleep(Duration::from_millis(50));
+        }
+    });
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![keypress_listener])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
