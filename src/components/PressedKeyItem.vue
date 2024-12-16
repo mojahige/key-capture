9baad229913026601e0c9db20e947fda2c4e19f3
mojahige <13248811+mojahige@users.noreply.github.com>
2024-12-16 10:12:01 +0900

    selectable color mode

---
+++ src/components/PressedKeyItem.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+defineProps<{
+  pressedKey: string;
+}>();
+</script>
+
+<template>
+<ins>
+  <code>
+    {{ pressedKey }}
+  </code>
+</ins>
+</template>
+
+<style scoped>
+ins {
+  --color: --font-color;
+  --background-color: --background-color;
+
+  flex: 0 0 auto;
+  display: grid;
+  place-items: center;
+  min-width: 3rem;
+  min-height: 3rem;
+  padding: 0.5rem;
+  background-color: var(--background-color);
+  border: 1px solid currentColor;
+  border-radius: 0.2rem;
+  color: currentColor;
+  font-size: 2rem;
+  font-family: monospace;
+  text-decoration: none;
+  text-transform: uppercase;
+  box-shadow: 0 2px currentColor;
+}
+</style>
---
+++ src/views/SettingView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ColorMode from "@/components/ColorMode.vue";
+
+</script>
+
+<template>
+<h1>Setting</h1>
+
+<ColorMode />
+</template>
